253322cde1e587963853b5fe467930b97f9ecca2
onixdevgmail <onix.develop@gmail.com>
2020-03-03 00:40:28 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # heinze
+
+pip install virtualenv
+virtualenv -p /usr/bin/python3.6 venv
+source venv/bin/activate
+
+pip install -r requirements.txt
+./manage.py migrate
+./manage.py collectstatic
+./manage.py runserver
--- heinze_site/views.py
+++ heinze_site/views.py
@@ -9,5 +9,12 @@ def index(request):
 
 def send_mail(request):
     if request.method == 'POST':
-        print(request)
+        data = request.POST
+        print(data)
+        firstname = data.get('firstname', None)
+        secondname = data.get('secondname', None)
+        firm = data.get('firm', None)
+        website = data.get('website', None)
+        email = data.get('email', None)
+        phonenumber = data.get('phonenumber', None)
     return redirect('index')
