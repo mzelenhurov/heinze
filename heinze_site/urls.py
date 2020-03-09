933b0ca63c3f017dbc5e7a1fa0cca68ddd797b08
onixdevgmail <onix.develop@gmail.com>
2020-03-09 10:44:22 +0200

    dynamic output for properties

--- heinze_site/urls.py
+++ heinze_site/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('send_mail', views.send_mail, name='send_mail'),
     path('upload_data', views.upload_data, name='upload_data'),
+    path('endpoint', views.endpoint, name='endpoint'),
     ]
